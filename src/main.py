11e044f45b8c541ffa8ab1e065888a540612e110
Кизянов Антон Олегович <kizianov.ao@svrauto.ru>
2025-05-25 17:35:27 +0300

    .

--- src/main.py
+++ src/main.py
@@ -669,7 +669,6 @@ class KCN:
                 headers=headers,
             )
             for response_dict in self.parse_bytes_to_dict(response_bytes)
-            for _ in self.logger_info(response_dict)
             for data_dataclass in self.convert_to_dataclass_from_dict(
                 ApiV3ProjectListGET.Res,
                 response_dict,
